3df989d71cfc61745f7246b8f40c896968b2daf5
tagyasu <42203711+tagyasu@users.noreply.github.com>
2020-02-21 20:03:09 +0900

    Update switch_structure.c

--- switch_structure.c
+++ switch_structure.c
@@ -1,7 +1,8 @@
 #define can_distance 5 //缶を収納したと判定する距離（単位はcm）
 #define can_pr //缶を区別するPRの閾値
 
-int mode =0;//モードを示す変数
+int mode = 0;//モードを示す変数
+int can_status = 0;//0:缶なし1:黒い缶あり2:銀色の缶あり
 
 void change_mode(){//モードを更新する関数
   if(mode == 1 && ultrasonic_sensor() < can_distance) mode++;カウンタが1の時に缶を検知するとインクリメント
@@ -31,7 +32,11 @@ void main(){
       linetrace(1,1,1);//前進、両側
     case 2:
       motor_brake();//ブレーキ//motor_stopでもいいかも
-      if(Read_canPR() > can_pr) 
+      if(Read_canPR() > can_pr){//缶の色を判定
+        can_status = 2;//銀
+      }else{
+        can_status = 1;//黒
+      }  
       linetrace(0,0,1);//後進、右側
     case 3:
       linetrace(0,0,1);//後進、右側
