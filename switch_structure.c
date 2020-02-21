0edf2b8cb1ef69ccb7b9642ae23d0ff13d9441f3
Kokilemon <58214906+Kokilemon@users.noreply.github.com>
2020-02-21 22:43:15 +0900

    Update switch_structure.c

--- switch_structure.c
+++ switch_structure.c
@@ -1,33 +1,37 @@
-#define can_distance 5 //缶を収納したと判定する距離（単位はcm）
-#define can_pr //缶を区別するPRの閾値
+#define D_stop (5)//缶を収納したと判定する距離（単位はcm）
+#define Pr3_stop (1000) //缶を区別するPRの閾値 実験で決める必要あり
 
 int mode = 0;//モードを示す変数
 int can_status = 0;//0:缶なし1:黒い缶あり2:銀色の缶あり
 
-void change_mode(){//モードを更新する関数
+void change_1to2(){//モードを更新する関数 // 初めに一回使うだけ
   if(mode == 1 && ultrasonic_sensor() < can_distance) mode++;//カウンタが1の時に缶を検知するとインクリメント
   if(pr1 > STOP || pr2 > STOP) mode++;//銀テープを検知するとカウンタをインクリメント
   if(mode == 9) mode = 1;
 }
-
-long Read_canPR(){//缶を判定するPRの値を読み取る関数
-  long sum = 0;
-  for (i=0; i<10; i++){//10回の平均をとる
-    AD.ADCSR.BIT.ADF = 0;// フラグクリア
-    AD.ADCSR.BIT.ADST = 1;//変換スタート
-    while(AD.ADCSR.BIT.ADF == 0);//変換が終わるまで待つ
-    sum += AD.ADDRC >> 6;//6bit右にずらしてsumに格納
-    AD.ADCSR.BIT.ADF = 0;//フラグクリア
-    quater_msecwait(); 
+void Read_Pr(int *p,int *q,int *r){
+  int i; long sum1; long sum2; long sum3; 
+  for ( i=0; i < 100; i++){
+    AD.ADCSR.BIT.ADF=0;  // フラグクリア
+    AD.ADCSR.BIT.ADST = 1;          //変換スタート
+    while(AD.ADCSR.BIT.ADF ==0);     //変換が終わるまで待つ
+    sum1 += AD.ADDRA >> 6;
+    sum2 += AD.ADDRB >> 6;     //6bit右にずらしてsumに格納
+    sum3 += AD.ADDRC >> 6;
+    AD.ADCSR.BIT.ADF = 0;    //フラグクリア
+    quater_msecwait();       //1msec wait  
   }
-  return sum/10;//読み取った値を返す
+  *p=sum1/100;  //pr1=sum1/100;と同じ
+  *q=sum2/100;  //pr2=sum2/100;と同じ
+  *r=sum3/100;
 }
-
 void main(){
+  int pr1; int pr2; int pr3;
+  Read_Pr(&pr1,&pr2,&pr3);
   
   change_mode();
   
-  switch(mode){
+    switch(mode){
     case 1:
       linetrace_init();
       linetrace(1,1,1);//前進、両側
